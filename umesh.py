0aa311f56d0b479ef76ad4fe9b641137a639c9a9
Umesh Samal <umeshsamal3@gmail.com>
2026-08-19 17:40:31 +0530

    Update umesh.py

--- umesh.py
+++ umesh.py
@@ -1,6 +1,22 @@
-python scripts\send_approval_email.py `
-  --approver "reviewer@example.com" `
-  --ontology "commercial-domain-model" `
-  --version "0.1.0" `
-  --commit "LOCAL-DEMO-001" `
-  --url "https://your-git-server/your-repository/commit/LOCAL-DEMO-001"
+
+
+ Validate ontology
+
+1. python scripts/validate_antology.py
+
+   Find reviewer
+
+2. python scripts/identify_approver.py
+  
+    send email
+3.  python scripts/send_approval_email.py --approver "umesh.samal_ext@novartis.com" --ontology "commercial-domain-model" --version "0.1.0" --commit "LOCAL-DEMO-001" --url "https://your-git-server/your-repository/commit/LOCAL-DEMO-001"
+
+4. Approval
+
+   python scripts/approval.py --approve --approver "reviewer@example.com" --commit "ASS"
+
+5 . Generate manifest file
+
+    python scripts/generate_manifest.py
+
+6. 
